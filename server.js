96fee92249d38812fa8cbf54ac46bccf8dd3a24f
BenS213 <57325318+BenS213@users.noreply.github.com>
2022-11-12 13:50:46 -0500

    added html routes.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const apiRoutes = require("./routes/apiRoutes");
-const HTMLRoutes = require("./routes/HTMLRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
-app.use('/', HTMLRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API is now listening on port ${PORT}!`)
