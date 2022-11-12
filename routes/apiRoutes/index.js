f50cdeac40b9c0beec2bf32b9c61d3a8f34f8448
BenS213 <57325318+BenS213@users.noreply.github.com>
2022-11-12 16:12:25 -0500

    add heroku

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,22 +1,28 @@
 const router = require('express').Router();
-const {createNewNote, updateDB} = require("../../lib/notes");
-const { v4: uuidv4 } = require('uuid');
-const {notes} = require("../../db/db.json");
+const { notes } = require("../../db/db.json");
+const fs = require('fs');
+const path = require('path');
+
 
 router.get("/notes", (req, res) => {
-    res.json(notes);
+    let results = notes;
+    res.json(results);
 });
 
 router.post("/notes", (req, res) => {
-    req.body.id = uuidv4();
-    const newNote = createNewNote(req.body, notes);
-    res.json(newNote);
+    req.body.id = notes.length.toString();
+    const note = req.body;
+    notes.push(note);
+    fs.writeFileSync(path.join(__dirname, '../../db/db.json'),
+        JSON.stringify({ notes: notes }, null, 2));
+    res.json(note);
 });
 
-router.delete(".notes/:id", (req, res) => {
-    const params = req.params.id;
-    updateDB(params, notes);
-    res.redirect('');
+router.delete('/notes/:id', (req, res) => {
+    let id = req.params.id;
+    notes.splice(id, 1);
+    notes.forEach(note => {note.id = JSON.stringify(notes.indexOf(note))});
+    res.json(notes);
 });
 
 module.exports = router;
